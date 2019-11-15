e3d25d092c896012027174f5e0f755c97a732b71
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-14 21:42:22 -0300

    Sintaxe simplificada que garante que os objetos IDisposable serão fechados.Objetos IDisposable não são gerenciados pelo clr,Eles precisam ser manualmente fechados.Exemplo font,FileStream,StreamReader,streamWriter

---
+++ CourseBlocoUsing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CourseBlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string origem = @"c:\temp\exemplo.txt";
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(origem))
+                {
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro,não foi encontrado esse arquivo ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
